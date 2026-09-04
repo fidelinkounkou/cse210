9d3d84987ece168b3fb2900ca65b59b12374d3eb
fidelinkounkou <fransemalonga@gmail.com>
2026-09-04 21:25:27 +0100

    changed something

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,16 +1,9 @@
 using System;
-using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
-        String last = Console.ReadLine();
-
-        Console.WriteLine($"your name is {last}, {first} {last}.");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
 }
